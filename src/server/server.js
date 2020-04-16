ee84c4e63ccb12e64172c0a8b24164d0b40ce723
lidia-saf <lidiia.safiullina@yandex.ru>
2020-04-15 22:46:40 -0700

    Add proxy to server

--- src/server/server.js
+++ src/server/server.js
@@ -1,13 +1,17 @@
 import path from 'path'
 import fs from 'fs'
 import cors from 'cors'
-
+import httpProxy from 'http-proxy';
 import express from 'express'
-const PORT = 3000;
-const app = express();
 
+const PORT = 8080;
+const app = express();
+const apiProxy = httpProxy.createProxyServer();
 
 app.use(cors());
+app.get("/api/*", function(req, res) {
+    apiProxy.web(req, res, { target: 'http://localhost:3001' });
+});
 
 app.use(express.static("dist"))
 
