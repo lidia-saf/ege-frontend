a5e518e58e6598c9b5f63791f3eb6462452e3974
lidia-saf <lidiia.safiullina@yandex.ru>
2020-04-17 18:11:15 -0700

    Change to ec2-user

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # update yum just in case
-sudo yum update -y
+yum update -y
 # get node into yum
 curl --silent --location https://rpm.nodesource.com/setup_10.x| bash -
 # install node and npm in one line
-sudo yum install -y nodejs
+yum install -y nodejs
 # install pm2 to restart node app
 npm i -g pm2@4.2.3
